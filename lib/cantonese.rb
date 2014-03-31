a8f5729cc45cec653570550690bd80f2f6e4522d
Francis Chong <francis@ignition.hk>
2014-03-31 17:05:34 +0800

    add require

--- lib/cantonese.rb
+++ lib/cantonese.rb
@@ -1,5 +1,7 @@
 require "cantonese/version"
+require 'cantonese/scraper/classified_scraper'
+require 'cantonese/scraper/radical_scraper'
+require 'cantonese/scraper/word_scraper'
 
 module Cantonese
-  # Your code goes here...
 end
